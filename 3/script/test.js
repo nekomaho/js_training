65e508d12c83cc813fb09c5fe9a97498940aa925
nekomaho <nekosatoru@gmail.com>
2019-07-21 09:32:38 +0900

    first commit

---
+++ 3/script/test.js
@@ -0,0 +1,19 @@
+const TAX=1.08
+let msg='Hello 世界';
+let x = 10;
+window.alert(msg);
+console.log(x*TAX);
+console.log('He\'s Hello');
+
+let new_msg='こんにちは\n 猫さん';
+window.alert(new_msg);
+
+let temp_str=`${msg}
+本当にいいね`;
+console.log(temp_str);
+
+let animal = ['猫','犬','豚'];
+console.log(animal[0]);
+
+let second_animal = ['猫',['犬'],'豚'];
+console.log(animal[1][0]);
