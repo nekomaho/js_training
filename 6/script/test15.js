e1c33d7b308987ee83ac4a1a429e9b4e1f52b10e
nekomaho <nekosatoru@gmail.com>
2019-08-18 16:10:07 +0900

    Chapter 5-4

--- 6/script/test15.js
+++ 6/script/test15.js
@@ -7,3 +7,44 @@ function* myGenerator(){
 for(let t of myGenerator()) {
     console.log(t);
 }
+
+function* getPrimes() {
+    let num = 2;
+    
+    while(true) {
+        if (isPrime(num)) { yield num; }
+        num++;
+    }
+}
+
+function isPrime(value) {
+    for (let i = 2; i < Math.floor(Math.sqrt(value)); i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for(let value of getPrimes()){
+    if (value > 100) { break; }
+    console.log(value);
+}
+
+class MyIterator {
+    constructor(data) {
+        this.data = data;
+        this[Symbol.iterator] = function* (){
+            let current = 0;
+            let that = this;
+            while(current < that.data.length) {
+                yield that.data[current++];
+            }
+        }
+    }
+}
+
+let ltr = new MyIterator(['one', 'two', 'three']);
+for(let value of ltr){
+    console.log(value);
+}
