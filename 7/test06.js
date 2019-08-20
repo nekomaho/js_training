8aeb1e3da2b7ff6e93eac92a997128771445b050
nekomaho <nekosatoru@gmail.com>
2019-08-20 22:04:00 +0900

    Use forEach

--- 7/test06.js
+++ 7/test06.js
@@ -1,8 +1,8 @@
 let s = document.getElementById('food');
 let opts = s.childNodes;
-for (let i=0, len = opts.length; i < len; i++) {
-    let opt = opts.item(i);
-    if (opt.nodeType === 1) {
-        console.log(opt.value);
+opts.forEach(function (value, key, parent) {
+    console.dir(value)
+    if (value.nodeType === 1) {
+        console.log(value.value);
     }
-}
+});
